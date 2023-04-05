f15b8ece26fb9f98bc8e8ebc8d1b72269e9f3541
Sarah Huggins <4612427+axium1998@users.noreply.github.com>
2023-04-05 18:13:42 -0500

    Updates

--- MaidMike/Commands/CmdModule.cs
+++ MaidMike/Commands/CmdModule.cs
@@ -1,4 +1,5 @@
-﻿using DSharpPlus;
+﻿using System.Text.RegularExpressions;
+using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using MaidMike.Helpers;
@@ -11,5 +12,15 @@ namespace MaidMike.Commands {
 			LogHelper.LogDebug("hit ping command"); 
 			await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("pong"));
 		}
-    }
+
+		[SlashCommand("roll", "Rolls a dice")]
+		public async Task RollCmd(InteractionContext ctx, [Option("roll", "The dice to roll")] string dice="1d20"){
+			LogHelper.LogDebug("hit roll command");
+			// create regex for +, -, and *, trimming whitespace, and splitting on the + and - signs
+			var regex = new Regex(@"\s*([+-/*])\s*");
+			var split = regex.Split(dice);
+			
+			await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent($"Rolling {dice}..."));
+		}
+	}
 }
--- MaidMike/Program.cs
+++ MaidMike/Program.cs
@@ -10,19 +10,12 @@ using Microsoft.Extensions.Logging;
 
 namespace MaidMike {
 	internal static class Program {
-		private static DiscordClient? _discord;
-		private static string? _token;
-		private static LogLevel _loglevel;
+		private static DiscordClient _discord = null!;
 
 		private static async Task Main() {
-			DoConfig();
+			BotInit();
 			
-			_discord = new DiscordClient(new DiscordConfiguration() {
-            	Token = _token,
-            	TokenType = TokenType.Bot,
-            	MinimumLogLevel = _loglevel,
-                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
-            });
+			if(_discord is null) throw new Exception("Discord bot failed to initialize");
 			
 			_discord.UseInteractivity(new InteractivityConfiguration() {
 				PollBehaviour = PollBehaviour.KeepEmojis,
@@ -32,31 +25,34 @@ namespace MaidMike {
 			var slash = _discord.UseSlashCommands();
 			slash.RegisterCommands<CmdModule>();
 
-			_discord.Logger.LogInformation(MaidEvents.MaiidMike, "MaidMike is starting up...");
+			LogHelper.Log( "MaidMike is starting up...");
 			await _discord.ConnectAsync();
 			
 			await Task.Delay(-1);
 		}
 		
-		private static void DoConfig() {
+		private static void BotInit() {
 			var lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\config.cfg");
-			
-			_token = (from x in lines select x.Split("=") into line where line[0].Trim()
-				.Equals("token") select line[1].Trim()).FirstOrDefault() ?? throw new Exception("Dunno how you got here, but you did");
 
-			_loglevel = (from x in lines select x.Split("=") into line where line[0].Trim()
-				.Equals("loglevel") select line[1].Trim().ToLower()).FirstOrDefault() switch {
-				"trace" => LogLevel.Trace,
-				"debug" => LogLevel.Debug,
-				"info" or "default" => LogLevel.Information,
-				"warning" or "warn" => LogLevel.Warning,
-				"error"=> LogLevel.Error,
-				"critical" => LogLevel.Critical,
-				"none" => LogLevel.None,
-				_ => throw new Exception("Invalid loglevel in config.cfg")
-			};
+			_discord = new DiscordClient(new DiscordConfiguration() {
+				TokenType = TokenType.Bot,
+				Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+				Token = (from x in lines select x.Split("=") into line where line[0].Trim()
+					.Equals("token") select line[1].Trim()).FirstOrDefault() ?? throw new Exception("Invalid token in config.cfg"),
+				MinimumLogLevel = (from x in lines select x.Split("=") into line where line[0].Trim()
+					.Equals("loglevel") select line[1].Trim().ToLower()).FirstOrDefault() switch {
+					"trace" => LogLevel.Trace,
+					"debug" => LogLevel.Debug,
+					"info" or "default" => LogLevel.Information,
+					"warning" or "warn" => LogLevel.Warning,
+					"error"=> LogLevel.Error,
+					"critical" => LogLevel.Critical,
+					"none" => LogLevel.None,
+					_ => throw new Exception("Invalid loglevel in config.cfg"),
+				}
+			});
 		}
 		
-		public static ILogger<BaseDiscordClient> Logger => _discord?.Logger ?? throw new Exception("Discord client not initialized");
+		public static ILogger<BaseDiscordClient> Logger => _discord.Logger ?? throw new Exception("Discord client not initialized");
 	}
 }
